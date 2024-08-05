c0ebecfbf213828bd28815862355d5b76a49bcd0
Sasakeyo <164603546+Sasakeyo@users.noreply.github.com>
2024-08-05 13:01:37 +0700

    Update wagmi.ts

--- src/wagmi.ts
+++ src/wagmi.ts
@@ -1,5 +1,6 @@
 import { defaultWagmiConfig } from '@web3modal/wagmi/react/config';
 import { fraxtal } from 'wagmi/chains';
+import { bscTestnet } from 'viem/chains'
 import { ADDRESSES } from './constants';
 
 export const projectId = process.env.NEXT_PUBLIC_WALLET_CONNECT_PROJECT_ID;
@@ -14,13 +15,14 @@ const metadata = {
 };
 
 const fraxtalVirtual = {
-  id: 252,
-  name: 'Fraxtal Virtual TestNet',
-  nativeCurrency: { name: 'Frax Ether', symbol: 'frxETH', decimals: 18 },
+  id: 97,
+  name: 'BNB Smart Chain Testnet',
+  nativeCurrency: { name: 'BNB', symbol: 'BNB', decimals: 18 },
   rpcUrls: {
     default: {
       http: [
-        'https://virtual.fraxtal.rpc.tenderly.co/e3156da7-ba03-40d3-b8cb-d8da12a27239',
+        'https://bsc-testnet-rpc.publicnode.com',
+        'https://data-seed-prebsc-2-s1.bnbchain.org:8545',
       ],
     },
   },
@@ -30,10 +32,10 @@ const fraxtalVirtual = {
       address: ADDRESSES.MULTICALL3,
     },
   },
-} as any as typeof fraxtal;
+} as any as typeof bscTestnet;
 
 export const chains = [
-  process.env.NEXT_PUBLIC_GIT_BRANCH === 'main' ? fraxtal : fraxtalVirtual,
+  process.env.NEXT_PUBLIC_GIT_BRANCH === 'main' ? bscTestnet : fraxtalVirtual,
 ] as const;
 
 export const config = defaultWagmiConfig({
