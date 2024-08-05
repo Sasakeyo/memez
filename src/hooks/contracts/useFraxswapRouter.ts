e5fc8ef224c284aa8ab0d50f1caee4aba97d086e
Sasakeyo <164603546+Sasakeyo@users.noreply.github.com>
2024-08-05 13:02:53 +0700

    Update useFraxswapRouter.ts

--- src/hooks/contracts/useFraxswapRouter.ts
+++ src/hooks/contracts/useFraxswapRouter.ts
@@ -3,7 +3,7 @@ import { IFraxswapRouter$Type } from '../../../artifacts/contracts/interfaces/fr
 import IFraxswapRouterJSON from '../../../artifacts/contracts/interfaces/fraxswap/IFraxswapRouter.sol/IFraxswapRouter.json';
 import { useMemo } from 'react';
 
-const routerAddress = '0x39cd4db6460d8B5961F73E997E86DdbB7Ca4D5F6' as Address;
+const routerAddress = '0xD99D1c33F9fC3444f8101754aBC46c52416550D1' as Address;
 
 export const useFraxswapRouterConfig = () => {
   return useMemo(
