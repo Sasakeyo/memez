a4b8baf4274650ac645a245ae962d59b2c7cf460
Sasakeyo <164603546+Sasakeyo@users.noreply.github.com>
2024-08-14 21:39:31 +0700

    Update wagmi.ts

--- src/wagmi.ts
+++ src/wagmi.ts
@@ -1,6 +1,6 @@
 import { defaultWagmiConfig } from '@web3modal/wagmi/react/config';
-import { fraxtal } from 'wagmi/chains';
-import { bscTestnet } from 'viem/chains'
+// import { fraxtal } from 'wagmi/chains';
+// import { bscTestnet } from 'viem/chains'
 import { ADDRESSES } from './constants';
 
 export const projectId = process.env.NEXT_PUBLIC_WALLET_CONNECT_PROJECT_ID;
@@ -14,17 +14,19 @@ const metadata = {
   icons: ['https://memez.me/icon.svg'],
 };
 
-const fraxtalVirtual = {
-  id: 97,
-  name: 'BNB Smart Chain Testnet',
-  nativeCurrency: { name: 'BNB', symbol: 'BNB', decimals: 18 },
+const mamoz = {
+  id: 7077,
+  name: 'Mamoz Token Factory',
+  nativeCurrency: { name: 'MMOZ', symbol: 'MMOZ', decimals: 18 },
   rpcUrls: {
     default: {
       http: [
-        'https://bsc-testnet-rpc.publicnode.com',
-        'https://data-seed-prebsc-2-s1.bnbchain.org:8545',
+        'https://rpc.mamoz.xyz',
       ],
     },
+  },
+  blockExplorers: {
+    default: { name: 'Explorer', url: 'https://exp.mamoz.xyz' },
   },
 
   contracts: {
@@ -32,10 +34,10 @@ const fraxtalVirtual = {
       address: ADDRESSES.MULTICALL3,
     },
   },
-} as any as typeof bscTestnet;
+};
 
 export const chains = [
-  process.env.NEXT_PUBLIC_GIT_BRANCH === 'main' ? bscTestnet : fraxtalVirtual,
+  mamoz,
 ] as const;
 
 export const config = defaultWagmiConfig({
@@ -43,5 +45,5 @@ export const config = defaultWagmiConfig({
   projectId,
   metadata,
   ssr: true,
-  enableEmail: true,
+  // enableEmail: true,
 });
