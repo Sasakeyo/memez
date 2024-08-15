0fec38d112407fb22d6f87cc352d542d994d5219
Sasakeyo <164603546+Sasakeyo@users.noreply.github.com>
2024-08-15 21:12:30 +0700

    Update useFraxswapFactory.ts

--- src/hooks/contracts/useFraxswapFactory.ts
+++ src/hooks/contracts/useFraxswapFactory.ts
@@ -3,7 +3,7 @@ import { IFraxswapFactory$Type } from '../../../artifacts/contracts/interfaces/f
 import IFraxswapFactoryJSON from '../../../artifacts/contracts/interfaces/fraxswap/IFraxswapFactory.sol/IFraxswapFactory.json';
 import { useMemo } from 'react';
 
-const factoryAddress = '0x6725F303b657a9451d8BA641348b6761A6CC7a17' as Address;
+const factoryAddress = '0xbA7D3bc45946C3F27C7b3e360a454c37Ecbd41DF' as Address;
 
 export const useFraxswapFactoryConfig = () => {
   return useMemo(
